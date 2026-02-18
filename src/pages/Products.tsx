3c95ef7eb765f50b15605566562fa8b0b824ba4a
jwolfish <148639710+jwolfish@users.noreply.github.com>
2026-02-17 23:23:45 -0600

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -638,6 +638,7 @@ function FertilizersList({ fertilizers, seasonId, onReload, showForm, onHideForm
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-500 focus:border-transparent"
                 >
                   <option value="gallon">Gallon</option>
+                  <option value="quart">Quart</option>
                   <option value="pound">Pound</option>
                 </select>
               </div>
